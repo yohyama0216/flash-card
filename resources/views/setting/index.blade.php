fbd07158fe15badea228fec3f1abf4f7d6e6fe97
大山陽平 <oyama_yohei@nanairo-inc.dev>
2023-02-04 20:07:08 +0900

    不要なもの削除

--- resources/views/setting/index.blade.php
+++ resources/views/setting/index.blade.php
@@ -8,14 +8,7 @@
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">学習設定</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
-            <div class="btn-group me-2">
-                <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
-            </div>
-            <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                <span data-feather="calendar"></span>
-                This week
-            </button>
+
         </div>
     </div>
 
@@ -23,15 +16,22 @@
         <div class="col-md-2 mb-3">
             <label for="country">出題数</label>
             <select class="custom-select d-block w-50" id="country" required="">
-                <option value="">Choose...</option>
-                <option>United States</option>
+            <option>5</option>
+                    <option>10</option>
+                    <option>15</option>
+                    <option>25</option>
+                    <option>50</option>
             </select>
         </div>
         <div class="col-md-2 mb-3">
             <label for="category">カテゴリー</label>
             <select class="custom-select d-block w-50" id="state" required="">
-                <option value="">Choose...</option>
-                <option>California</option>
+            <option value="0">すべて</option>
+                        <option>5</option>
+                        <option>10</option>
+                        <option>15</option>
+                        <option>25</option>
+                        <option>50</option>
             </select>
         </div>
     </div>
@@ -39,15 +39,19 @@
         <div class="col-md-2 mb-3">
             <label for="word_limit">単語数上限</label>
             <select class="custom-select d-block w-50" id="state" required="">
-                <option value="">Choose...</option>
-                <option>California</option>
+            <option>8</option>
+                    <option>15</option>
+                    <option>25</option>
             </select>
             </div>
             <div class="col-md-2 mb-3">
             <label for="last_learn_date">前回学習日</label>
             <select class="custom-select d-block w-50" id="state" required="">
-                <option value="">Choose...</option>
-                <option>California</option>
+            <option>5日前</option>
+                    <option>10</option>
+                    <option>15</option>
+                    <option>25</option>
+                    <option>50</option>
             </select>
         </div>
     </div>
@@ -55,53 +59,6 @@
               <div class="col-md-2">
             <button>設定を保存</button>
         </div>
-            </div>
-    <div class="row">
-        <div class="col-4">
-            <ul>
-            <li>
-                <span>出題数</span>
-                <select>
-                    <option>5</option>
-                    <option>10</option>
-                    <option>15</option>
-                    <option>25</option>
-                    <option>50</option>
-                </select>
-            </li>
-            <li>
-                <span>単語数上限</span>
-                <select>
-                    <option>8</option>
-                    <option>15</option>
-                    <option>25</option>
-                </select>
-            </li>    
-            <li>
-                <span>前回学習日</span>
-                <select>
-                <option>なし</option>
-                    <option>5日前</option>
-                    <option>10</option>
-                    <option>15</option>
-                    <option>25</option>
-                    <option>50</option>
-                </select>
-            </li>
-                <li>
-                    <span>ジャンル</span>
-                    <select>
-                        <option value="0">すべて</option>
-                        <option>5</option>
-                        <option>10</option>
-                        <option>15</option>
-                        <option>25</option>
-                        <option>50</option>
-                    </select>
-                </li>
-            </ul>
-        </div>
-
     </div>
 </main>
 @endsection
